ddbb1c59c05ed67ce6d81742a6b7db1480deaeb1
uirysousa <uiry.sousa@hotmail.com>
2024-07-03 09:28:41 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+function abrirModal(){
+    overlay.classList.add("active");
+    criarTarefa.classList.add("active");
+}
+
+function fecharModal(){
+    overlay.classList.remove("active");
+    criarTarefa.classList.remove("active");
+}
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+        inserirTarefas(res);
+    })
+} buscarTarefas();
+
+function inserirTarefas(listaDeTarefas){
+    if(listaDeTarefas.length > 0){
+        lista.innerHTML = ""
+        listaDeTarefas.map(tarefa =>{
+            lista.innerHTML += `
+            <li>
+                <h5>${tarefa.titulo}</h5>
+                <p>${tarefa.descricao}</p>
+                <div class="actions">
+                    <box-icon name='trash' size="sm"></box-icon>
+                </div>
+            </li>             
+            `;
+        })
+    }
+}
+function novaTarefa(){
+    event.preventDefault(); // para nao recarregar a pagina
+    let tarefa = {
+        titulo: titulo.value,
+        descricao:descricao.value
+    }
+    fetch("http://localhost:3000/tarefas",{
+        method:"POST",
+        headers:{
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify(tarefa) //transforma em hipertexto
+    })
+    .then(res => res.json())
+    .then(res => {
+        fecharModal();
+        buscarTarefas();
+    })  
+} 
