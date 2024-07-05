1adfc9a05dc8e36dac15d20737dd37bc1e9675ed
uirysousa <uiry.sousa@hotmail.com>
2024-07-05 20:41:55 -0300

    projeto finalizado
    parte final do projeto

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function inserirTarefas(listaDeTarefas){
                 <h5>${tarefa.titulo}</h5>
                 <p>${tarefa.descricao}</p>
                 <div class="actions">
-                    <box-icon name='trash' size="sm"></box-icon>
+                    <box-icon name='trash' size="sm" onclick="deletarTarefa(${tarefa.id})"></box-icon>
                 </div>
             </li>             
             `;
@@ -48,5 +48,33 @@ function novaTarefa(){
     .then(res => {
         fecharModal();
         buscarTarefas();
+        let form = document.querySelector("#criarTarefa form");
+        form.reset();
     })  
 } 
+function deletarTarefa(id){
+    fetch(`http://localhost:3000/tarefas/${id}`,{
+        method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(res =>{
+        alert("Tarefa deletada com sucesso")
+        buscarTarefas();
+    })
+}
+function pesquisarTarefas(){
+    let lis =document.querySelectorAll("ul li");  
+    if(busca.value.length > 0){
+            lis.forEach(li => {
+            if(!li.children[0].innerText.includes(busca.value)){
+                li.classList.add('oculto'); 
+            }else{
+            li.classList.remove('oculto'); 
+            } 
+        })
+    } else {
+        lis.forEach(li => {  
+            li.classList.remove('oculto');   
+        })
+    }    
+}
